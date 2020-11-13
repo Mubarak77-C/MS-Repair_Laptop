92f29be7705a1fc35dec319f16b1577262ed172f
Mubarak77-C <59537691+Mubarak77-C@users.noreply.github.com>
2020-11-14 01:14:29 +0500

    Routing changes switch to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import {  HashRouter, Route, Link  } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./component/Home"
 // import Slide from "./component/Slide"
@@ -19,14 +19,14 @@ const App = () => {
   return (
 
     <>
-
+<HashRouter basename="/">
       <Switch>
-        <Route exact path="https://mubarak77-c.github.io/MS-Repair_Laptop/" component={Home} />
-        <Route path="https://mubarak77-c.github.io/MS-Repair_Laptop/about" component={About} />
-        <Route path="https://mubarak77-c.github.io/MS-Repair_Laptop/contact" component={Contact} />
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
         {/* <Route path="/submit" component={Submit} /> */}
         <Route component={Error} />
-      </Switch>
+      </HashRouter>
 
     </>
   )
